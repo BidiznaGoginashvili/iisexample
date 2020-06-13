eee4e11dd61f2ec892f18c3eb1bc6772d130e39c
BidiznaGoginashvili <45213809+BidiznaGoginashvili@users.noreply.github.com>
2020-06-13 19:15:41 +0400

    Add project files.

---
+++ WCFIISExample/ExampleService.svc.cs
@@ -0,0 +1,12 @@
+﻿namespace WCFIISExample
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ExampleService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ExampleService.svc or ExampleService.svc.cs at the Solution Explorer and start debugging.
+    public class ExampleService : IExampleService
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
